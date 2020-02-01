b568d85e3170fca37c830d61aad11550b0c38c85
loganda.smith@ufl.edu <loganda.smith@ufl.edu>
2020-02-01 17:01:56 -0500

    Added Boilerplate for JobCards

---
+++ swamp-app/src/componenets/JobCards.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductCard from react-ui-cards;
+
+function JobCards() {
+    return (
+        <div className="JobCards">
+            <ProductCard
+                photos = {[
+                    
+                ]}
+            />
+
+        </div>
+    )
+}
